8d5094fa3cb028cae5d895eab89d22952b81523a
Kapitanov Ivan <kapivano@gmail.com>
2023-06-12 20:54:22 +0300

    Finish taskpage

--- lib/pages/taskpage.dart
+++ lib/pages/taskpage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+enum Priority{
+  none, low, high
+}
 
 class TaskPage extends StatefulWidget {
   const TaskPage({Key? key}) : super(key: key);
@@ -11,8 +16,10 @@ class _TaskPageState extends State<TaskPage> {
   var priority = [
     'None',
     'Low',
-    'High',
+    '!!High',
   ];
+  bool _switch = false;
+  DateTime? date;
 
   String dropdownValue = 'None';
 
@@ -39,53 +46,121 @@ class _TaskPageState extends State<TaskPage> {
               },
               child: const Text(
                 'Save',
-                style: TextStyle(color: Colors.blue),
+                style: TextStyle(color: Colors.blue, fontSize: 14),
               )),
         ],
       ),
       body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                     hintText: 'Task to do',
+                    filled: true,
+                    fillColor: Colors.white),
+                autofocus: false,
+                maxLines: null,
+                keyboardType: TextInputType.text,
+                minLines: 4,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 15),
+              child: Text('Priority', style: TextStyle(fontSize: 16)),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 16),
+              child: DropdownButton<String>(
+                items: priority.map((String items) {
+                  return DropdownMenuItem(
+                    value: items,
+                    child: Text(
+                      items,
+                      style: TextStyle(
+                          color: items == '!!High' ? Colors.red : Colors.black),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  // This is called when the user selects an item.
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_drop_down_rounded),
+                isExpanded: true,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 16),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Make by date',
+                          style: TextStyle(fontSize: 16)),
+                      Text(
+                          date != null
+                              ? DateFormat('yyyy-MM-dd').format(date!)
+                              : '',
+                          style:
+                              const TextStyle(fontSize: 12, color: Colors.blue))
+                    ],
                   ),
-                  autofocus: false,
-                  maxLines: null,
-                  keyboardType: TextInputType.text,
-                  minLines: 4,
-                ),
+                  const Spacer(),
+                  Switch(
+                      value: _switch,
+                      onChanged: (bool value) async {
+                        setState(() {
+                          _switch = value;
+                        });
+                        if (value) {
+                          DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              //get today's date
+                              firstDate: DateTime.now(),
+                              //DateTime.now() - not to allow to choose before today.
+                              lastDate: DateTime(2101));
+                          if (pickedDate != null) {
+                            setState(() {
+                              date = pickedDate;
+                            });
+                          } else {
+                            setState(() {
+                              _switch = false;
+                            });
+                          }
+                        } else {
+                          setState(() {
+                            date = null;
+                          });
+                        }
+                      })
+                ],
               ),
-              const Padding(
-                padding: EdgeInsets.only(left: 8),
-                child: Text('Priority', style: TextStyle(fontSize: 16)),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: DropdownButton<String>(
-                  items: priority.map((String items) {
-                    return DropdownMenuItem(
-                      value: items,
-                      child: Text(items, style: TextStyle(color: items=='High'? Colors.red : Colors.black ),),
-                    );
-                  }).toList(),
-                  onChanged: (String? value) {
-                    // This is called when the user selects an item.
-                    setState(() {
-                      dropdownValue = value!;
-                    });
+            ),
+            const Divider(color: Colors.black),
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: TextButton.icon(
+                  onPressed: () {
+                    return;
                   },
-                  value: dropdownValue,
-                  icon: const Icon(Icons.arrow_drop_down_rounded),
-                  isExpanded: true,
-                ),
+                  icon: const Icon(Icons.delete, color: Colors.red),
+                  label: const Text('Delete', style: TextStyle(fontSize: 16, color: Colors.red))
               ),
-            ],
-          ),
+            )
+          ],
+        ),
       ),
       backgroundColor: const Color(0xFFf7f6f2),
     );
