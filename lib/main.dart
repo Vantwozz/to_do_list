4830f5373d5dc291acecef258c79710354337f98
Kapitanov Ivan <kapivano@gmail.com>
2023-06-12 22:14:14 +0300

    add initial values, finish taskpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/pages/homepage.dart';
 import 'package:to_do_list/pages/taskpage.dart';
+import 'package:to_do_list/include/task.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: TaskPage(),
+    MaterialApp(
+      home: TaskPage(Task('To do smthing',Priority.high, true ,DateTime(2023)), false),
     )
   );
 }
