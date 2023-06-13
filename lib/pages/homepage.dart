e38b1285ba16bd3f3d5a2d9f010ec550fa227338
Kapitanov Ivan <kapivano@gmail.com>
2023-06-13 13:50:33 +0300

    made list in homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/pages/taskpage.dart';
 import 'package:to_do_list/include/task.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,7 +13,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<Task> toDoList = [
     Task('something', Priority.none, false),
-    Task('another task', Priority.low, false, DateTime.now())
+    Task(
+        'another task loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong',
+        Priority.low,
+        false,
+        DateTime.now()),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
+    Task('something', Priority.none, false),
   ];
 
   @override
@@ -86,6 +107,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
+        controller: ScrollController(),
         slivers: <Widget>[
           const SliverAppBar(
             pinned: true,
@@ -100,77 +122,120 @@ class _HomePageState extends State<HomePage> {
           ),
           SliverToBoxAdapter(
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
-              child: Material(
-                borderRadius: BorderRadius.circular(8.0),
-                elevation: 2,
-                child: Column(
-                  children: [
-                    ListView.builder(
-                      padding: const EdgeInsets.only(top: 0),
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Dismissible(
-                          key: UniqueKey(),
-                          background: Container(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 20),
-                            alignment: AlignmentDirectional.centerStart,
-                            color: Colors.red.shade300,
-                            child: const Icon(
-                              Icons.delete,
-                              color: Colors.white,
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                child: Material(
+                  borderRadius: BorderRadius.circular(8.0),
+                  elevation: 2,
+                  child: Column(
+                    children: [
+                      ListView.builder(
+                        controller: ScrollController(),
+                        padding: const EdgeInsets.only(top: 0),
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Dismissible(
+                            key: UniqueKey(),
+                            secondaryBackground: Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20),
+                              alignment: AlignmentDirectional.centerStart,
+                              color: Colors.red.shade300,
+                              child: const Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                          secondaryBackground: Container(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 20),
-                            alignment: AlignmentDirectional.centerEnd,
-                            color: Colors.lightGreen,
-                            child: const Icon(
-                              Icons.check_rounded,
-                              color: Colors.white,
+                            background: Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20),
+                              alignment: AlignmentDirectional.centerEnd,
+                              color: Colors.lightGreen,
+                              child: const Icon(
+                                Icons.check_rounded,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                          onDismissed: (direction) {
-                            setState(() {
-                              handleDismiss(direction, index);
-                            });
-                          },
-                          confirmDismiss: (direction) =>
-                              promptUser(direction),
-                          child: Material(
-                            color: Colors.white,
-                            borderRadius: index == 0
-                                ? const BorderRadius.vertical(
-                                    bottom: Radius.circular(0.0),
-                                    top: Radius.circular(8.0))
-                                : null,
-                            child:
-                                ListTile(title: Text(toDoList[index].text!)),
-                          ),
-                        );
-                      },
-                      itemCount: toDoList.length,
-                    ),
-                    Material(
-                      borderRadius: const BorderRadius.vertical(
-                          top: Radius.circular(0.0),
-                          bottom: Radius.circular(8.0)),
-                      child: TextButton(
-                        onPressed: () {
-                          return;
+                            onDismissed: (direction) {
+                              setState(() {
+                                handleDismiss(direction, index);
+                              });
+                            },
+                            confirmDismiss: (direction) =>
+                                promptUser(direction),
+                            child: Material(
+                                color: Colors.white,
+                                borderRadius: index == 0
+                                    ? const BorderRadius.vertical(
+                                        bottom: Radius.circular(0.0),
+                                        top: Radius.circular(8.0))
+                                    : null,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            Checkbox(
+                                              value: toDoList[index].done,
+                                              onChanged: (bool? value) {},
+                                            ),
+                                            Expanded(
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(toDoList[index].text!,
+                                                      maxLines: 3,
+                                                      style: const TextStyle(
+                                                          fontSize: 16),
+                                                      overflow: TextOverflow
+                                                          .ellipsis),
+                                                  Text(
+                                                    toDoList[index].date != null
+                                                        ? DateFormat(
+                                                                'yyyy-MM-dd')
+                                                            .format(
+                                                                toDoList[index]
+                                                                    .date!)
+                                                        : '',
+                                                    style: const TextStyle(
+                                                        color: Color.fromRGBO(
+                                                            0, 0, 0, 0.3),
+                                                        fontSize: 14),
+                                                  )
+                                                ],
+                                              ),
+                                            )
+                                          ]),
+                                    ),
+                                    IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.info_outline)),
+                                  ],
+                                )),
+                          );
                         },
-                        style: TextButton.styleFrom(
-                            backgroundColor: Colors.white),
-                        child: const Text('Add'),
-                      )
-                    ),
-                  ],
-                ),
-              )
-            ),
+                        itemCount: toDoList.length,
+                      ),
+                      Material(
+                          borderRadius: const BorderRadius.vertical(
+                              top: Radius.circular(0.0),
+                              bottom: Radius.circular(8.0)),
+                          child: TextButton(
+                            onPressed: () {
+                              return;
+                            },
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.white),
+                            child: const Text('Add'),
+                          )),
+                    ],
+                  ),
+                )),
           )
         ],
       ),
