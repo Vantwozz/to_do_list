4c52af39e0b1d4619b56aaf1e8348a78ee56b15e
Kapitanov Ivan <kapivano@gmail.com>
2023-07-13 21:57:15 +0300

    added crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:to_do_list/domain/utils.dart';
+import 'package:to_do_list/firebase_options.dart';
 import 'package:to_do_list/navigation/navigation.dart';
 import 'package:to_do_list/navigation/routes.dart';
 import 'package:to_do_list/theme/theme_constants.dart';
 
 import 'locator.dart';
 
-void main() {
+void main() async {
   setup();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   FlutterError.onError = (errorDetails) {
     MyLogger.l.d('Error in FlutterError');
     FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
